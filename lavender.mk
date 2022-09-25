48257e1524218282f6fb205866ef06442d81203d
Moonitime: The Yandere <moonitimetheyandere@gmail.com>
2022-09-25 15:55:39 +0000

    lavender: Zephyrus-ify!
    Signed-off-by: Shizuka Hinode <moonitimetheyandere@gmail.com>

--- lavender.mk
+++ lavender.mk
@@ -8,26 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common RiceDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Project Zephyrus flags
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# riceDroid Flags
-RICE_MAINTAINER := JojiOnThatBeat•ShizukaHinode
-RICE_OFFICIAL := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_ENABLE_BLUR := true
-
-# Arrow GApps
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
